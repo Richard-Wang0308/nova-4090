a7045e3c6f8d77d25bc29e3424c8d6b3cd85ad4d
Richard-Wang0308 <richard19900308@gmail.com>
2026-02-12 02:08:05 +0000

    mutate miner code added

--- run.sh
+++ run.sh
@@ -9,7 +9,16 @@ pm2 start "python3 neurons/miner_mini2.py --wallet.name multisig-jjpes-shib --wa
 CUDA_VISIBLE_DEVICES=0,1 pm2 start "python3 neurons/miner_mini2.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotf --gpu_ids '1' --logging.debug" --name "rxn2_miner"
 
 CUDA_VISIBLE_DEVICES=0 pm2 start "python3 neurons/miner_mini1.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotc --logging.debug" --name "rxn5_miner"
-CUDA_VISIBLE_DEVICES=0 python3 neurons/miner_mini1.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotf --logging.debug
+
+CUDA_VISIBLE_DEVICES=1 pm2 start "python3 neurons/miner_mini1.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug" --name "rxn5_miner"
+
+CUDA_VISIBLE_DEVICES=0 python3 neurons/miner_mini1.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotc --logging.debug
 
 CUDA_VISIBLE_DEVICES=0 python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotc --logging.debug
+
+
+CUDA_VISIBLE_DEVICES=0 python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hote --logging.debug
+CUDA_VISIBLE_DEVICES=0 python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotd --logging.debug
+CUDA_VISIBLE_DEVICES=0 python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
+CUDA_VISIBLE_DEVICES=0 python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hota --logging.debug
 
