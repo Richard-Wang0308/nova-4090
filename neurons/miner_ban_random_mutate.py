7d4fe2118c660cc84b8d0df0d4560cb383d00ce8
Richard-Wang0308 <richard19900308@gmail.com>
2026-02-18 01:48:06 +0000

    db updated

--- neurons/miner_ban_random_mutate.py
+++ neurons/miner_ban_random_mutate.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASE_DIR)
 
 DB_PATH = os.path.join(BASE_DIR, "combinatorial_db", "molecules.sqlite")
-HARDCODED_RXN_ID = 1
+HARDCODED_RXN_ID = 2
 STARTING_EPOCH = 20934
 REACTION_TRAIN_CSV = os.path.join(BASE_DIR, 'data', 'mols.csv')
 SCORE_RESULTS_DB = os.path.join(BASE_DIR, "..", "nova-4090", "score_results.sqlite")
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 pm2 start "python3 neurons/miner_ban_mini.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug" --name "miner_ban_mini"
+pm2 start "python3 neurons/miner_ban_synthon.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug" --name "miner_ban_synthon"
 pm2 start "python3 neurons/miner_ban_random_mutate.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug" --name "miner_ban_random_mutate"
 
 python3 neurons/miner_ban_mini.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
 python3 neurons/miner_ban_synthon.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
 python3 neurons/miner_ban_neighbour_mutate.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
 python3 neurons/miner_ban_random_mutate.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
-python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hotb --logging.debug
+python3 neurons/simple_submit.py --wallet.name multisig-jjpes-shib --wallet.hotkey hota --logging.debug
