966936674b47f51a18b5a7512400c3c18ff5d5b6
Richard-Wang0308 <richard19900308@gmail.com>
2026-03-13 13:59:41 +0000

    db added

--- neurons/gentle_data.py
+++ neurons/gentle_data.py
@@ -70,8 +70,8 @@ def validate_molecule_heavy_atoms(
     """Validate heavy atom count."""
     try:
         heavy_atom_count = get_heavy_atom_count(smiles)
-        min_atoms = 20
-        max_atoms = 24
+        min_atoms = 18
+        max_atoms = 26
 
         if heavy_atom_count < min_atoms:
             return False, f"Insufficient heavy atoms: {heavy_atom_count} < {min_atoms}"
@@ -405,8 +405,8 @@ def load_molecules_from_db_with_validation(
                     continue
                 
                 # Check heavy atom count
-                min_heavy_atoms = 20
-                max_heavy_atoms = 24
+                min_heavy_atoms = 18
+                max_heavy_atoms = 26
                 heavy_atom_count_val = get_heavy_atom_count(smiles)
                 if heavy_atom_count_val < min_heavy_atoms:
                     logger.debug(f"Molecule {molecule_name} has insufficient heavy atoms ({heavy_atom_count_val} < {min_heavy_atoms}), skipping")
@@ -542,8 +542,8 @@ def load_molecules_from_csv_with_validation(
                 
                 # Check heavy atom count
                 # min_heavy_atoms = config.get('min_heavy_atoms', 10)
-                min_heavy_atoms = 20
-                max_heavy_atoms = 24
+                min_heavy_atoms = 18
+                max_heavy_atoms = 26
 
                 heavy_atom_count_val = get_heavy_atom_count(smiles)
                 if heavy_atom_count_val < min_heavy_atoms:
