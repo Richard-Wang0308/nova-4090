034dfd326dbdf7b60ad65c952079554c61c28094
Richard-Wang0308 <richard19900308@gmail.com>
2026-03-15 19:25:32 +0000

    data added

--- neurons/mini_data.py
+++ neurons/mini_data.py
@@ -17,7 +17,7 @@ sys.path.append(BASE_DIR)
 
 DB_PATH = os.path.join(BASE_DIR, "combinatorial_db", "molecules.sqlite")
 HARDCODED_RXN_ID = 5
-STARTING_EPOCH = 21353
+STARTING_EPOCH = 21458
 REACTION_TRAIN_CSV = os.path.join(BASE_DIR, 'data', 'mols.csv')
 SCORE_RESULTS_DB = os.path.join(BASE_DIR, "score_results.sqlite")
 
@@ -66,8 +66,8 @@ def validate_molecule_heavy_atoms(
     try:
         heavy_atom_count = get_heavy_atom_count(smiles)
         min_atoms = config.get('min_heavy_atoms', 10)
-        min_atoms = 20
-        max_atoms = 24
+        min_atoms = 18
+        max_atoms = 29
 
         if heavy_atom_count < min_atoms:
             return False, f"Insufficient heavy atoms: {heavy_atom_count} < {min_atoms}"
@@ -479,8 +479,8 @@ def load_molecules_from_db_with_validation(
                 
                 # Check heavy atom count
                 # min_heavy_atoms = config.get('min_heavy_atoms', 10)
-                min_heavy_atoms = 20
-                max_heavy_atoms = 24
+                min_heavy_atoms = 18
+                max_heavy_atoms = 29
                 heavy_atom_count_val = get_heavy_atom_count(smiles)
                 if heavy_atom_count_val < min_heavy_atoms:
                     logger.debug(f"Molecule {molecule_name} has insufficient heavy atoms ({heavy_atom_count_val} < {min_heavy_atoms}), skipping")
