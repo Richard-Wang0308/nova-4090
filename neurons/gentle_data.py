aa77e4dddd8ff8fd48f0407eef81596efae238f5
Richard-Wang0308 <richard19900308@gmail.com>
2026-04-01 12:41:05 +0000

    added

--- neurons/gentle_data.py
+++ neurons/gentle_data.py
@@ -17,8 +17,8 @@ sys.path.append(BASE_DIR)
 
 DB_PATH = os.path.join(BASE_DIR, "combinatorial_db", "molecules.sqlite")
 HARDCODED_RXN_ID = 5
-# STARTING_EPOCH = 21771
-STARTING_EPOCH = 21786
+STARTING_EPOCH = 21771
+# STARTING_EPOCH = 21786
 REACTION_TRAIN_CSV = os.path.join(BASE_DIR, 'data', 'mols.csv')
 SCORE_RESULTS_DB = os.path.join(BASE_DIR, "score_results.sqlite")
 
@@ -1236,7 +1236,7 @@ async def run_generation_and_scoring_loop_enhanced(state: Dict[str, Any]) -> Non
                 continue
             
             # top_molecules_df = molecules_df[30:500]
-            top_molecules_df = molecules_df.head(200)
+            top_molecules_df = molecules_df.head(300)
             state['top_pool'] = molecules_df.copy()
             state['seen_inchikeys'].update(molecules_df['InChIKey'].tolist())
             
