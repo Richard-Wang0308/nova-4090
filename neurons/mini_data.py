4de36aee7b81da4d44045abd9d40a2ff9e594046
Richard-Wang0308 <richard19900308@gmail.com>
2026-07-11 10:28:08 +0000

    updated

--- neurons/mini_data.py
+++ neurons/mini_data.py
@@ -16,10 +16,10 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASE_DIR)
 
 DB_PATH = os.path.join(BASE_DIR, "combinatorial_db", "molecules.sqlite")
-HARDCODED_RXN_ID =  4
+HARDCODED_RXN_ID =  1
 STARTING_EPOCH = 23724
-REACTION_TRAIN_CSV = os.path.join(BASE_DIR, 'data', 'rxn4.csv')
-SCORE_RESULTS_DB = os.path.join(BASE_DIR, "score_results_4.sqlite")
+REACTION_TRAIN_CSV = os.path.join(BASE_DIR, 'data', 'rxn1.csv')
+SCORE_RESULTS_DB = os.path.join(BASE_DIR, "score_results_1.sqlite")
 
 from config.config_loader import load_config
 from utils import (
