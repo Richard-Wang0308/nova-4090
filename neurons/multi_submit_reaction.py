ab9469190878252d3afba21b0de4871d37277359
Richard_Wang0308 <richard19900308@gmail.com>
2026-07-22 00:33:35 +0000

    updated

--- neurons/multi_submit_reaction.py
+++ neurons/multi_submit_reaction.py
@@ -47,8 +47,8 @@ sys.path.append(BASE_DIR)
 # Add as many wallets/hotkeys as needed.
 # ============================================================================
 WALLET_HOTKEY_PAIRS: List[Tuple[str, str]] = [
-    ("nova",   "notb"),
-    ("nova",   "nota"),
+    ("nova",   "notd"),
+    ("nova",   "notc"),
     # ("nova",   "notd")
     # ("alpha",  "hotkey1"),
     # ("beta",   "hotkey1"),
